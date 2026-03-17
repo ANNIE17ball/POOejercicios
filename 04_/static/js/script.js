a0e6fd9511f9a57664904a6525be2c96937acc07
ANNIE17ball <annerillanabalon@liceovvh.cl>
2026-03-17 12:57:47 -0300

    por fiiin

--- 04_/static/js/script.js
+++ 04_/static/js/script.js
@@ -1,9 +1,13 @@
-function contador() {
+let asistencia = [];
+
+function registrarAlumno() {
     const container = document.getElementById(`resultado1`);
     const parrafo = document.getElementById(`pantalla1`);
+    let input = document.getElementById(`val1`).value; // capturar valor del input
+    //input = parseInt(input); --> Transformar a numero
+    asistencia.push(input);
 
-    parrafo.textContent = ""; //limpiar contenedor
-    
-    parrafo.textContent = ``;
+    parrafo.innerHTML = `Alumnos presentes: <br> ${asistencia.join(`<br>`)}`;
     container.classList.remove(`d-none`);
+    document.getElementById(`val1`).value = "";
 }
